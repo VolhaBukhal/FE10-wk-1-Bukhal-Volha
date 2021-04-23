3613ad20b7bfd3ef13725611bccb3a355effccd2
Volha <o.bukhal@gmail.com>
2021-04-24 02:37:24 +0300

    works section without slick slider

---
+++ landing-2/js/skript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.works').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+  });
+
+ 
+
