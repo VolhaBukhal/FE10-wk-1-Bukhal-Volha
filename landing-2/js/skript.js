97be30f2ece5ae9af2bb4a7c9ab1e5ee9d9e23a5
Volha <o.bukhal@gmail.com>
2021-04-25 02:44:48 +0300

    works section with slider

--- landing-2/js/skript.js
+++ landing-2/js/skript.js
@@ -1,12 +1,50 @@
-$(document).ready(function(){
-    $('.works').slick({
+// $(document).ready(function(){
+//     $('.works').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear',
+//         slidesToShow: 4,
+//         responsive: [
+//             {
+//                 breakpoint: 800,
+//                 settings: "unslick"
+//             }
+//         ]
+//     });
+//   });
+// !!! Get code from stackoverflow Above code don't work.
+var sliderElem = $(".works"),
+    sliderBool = false,
+    sliderBreakpoint = 768,
+    sliderSettings = {
+        arrows: true,
         dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-      });
-  });
+        autoplaySpeed: 3000,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: sliderBreakpoint,
+                settings: "unslick"
+            }
+        ]
+    };
+function sliderInit() {
+    if (window.innerWidth <= sliderBreakpoint) {
+        if (sliderBool == false) {
+            sliderElem.slick(sliderSettings);
+            sliderBool = true;
+        }
+    } else {
+        sliderBool = false;
+    }
+}
+
+// resize
+$(window).resize(function () {
+    sliderInit();
+});
 
  
 
