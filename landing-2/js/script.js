98cee31c3181db4a7680ba9c41513c8dbb24f033
Olga <o.bukhal@gmail.com>
2021-05-04 15:24:09 +0300

    footer divied

--- landing-2/js/script.js
+++ landing-2/js/script.js
@@ -38,19 +38,44 @@
     //   });
 
 $(document).ready(function(){
+
+  //fixing the header
+  let header = $('#header');
+  let firstscreen = $('#firstscreen');
+   
+  // getting scrall position
+  let scrollPos = $(window).scrollTop();
+//scroll load resize - делаем проверку при прокрутке, изменении размеров, загрузке
+  $(window).on("scroll load resize", function(){
+
+    //getting the height of section firstscreen withot padding
+    let firstscreenHeight = firstscreen.innerHeight();
+    scrollPos = $(this).scrollTop();
+    if( scrollPos > firstscreenHeight){
+     
+      header.addClass("fixed"); //adding class .fixed
+
+    } else {
+
+      header.removeClass("fixed"); //removing class .fixed
+
+    }
+  });
+
   //burger initializing
   $('.burger').click(function(event){
     $('.burger, .header-nav__menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
 
- //slick slider initializing
+ //slick slider initializing  https://kenwheeler.github.io/slick/
   $('.feeedback-slider').slick({
     dots: true,
     arrows: false,
     infinite: true,
     speed: 1200,
-    slidesToShow: 1
+    slidesToShow: 1,
+    adaptiveHeight: true
   });
 });
 
